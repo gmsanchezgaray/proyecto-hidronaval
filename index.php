4489fe0b04261a1cb063f855bf5933f864218864
Santi <it.santiagotoledo@gmail.com>
2021-02-12 05:47:54 -0300

    Modificacion seccion nosotros

--- index.php
+++ index.php
@@ -97,7 +97,21 @@
 </section>
 <main>
     <div class="container">
-        <section class="nosotros d-flex justify-content-evenly">
+        <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+                <div class="d-flex flex-column justify-content-evenly h-100">
+                    <span class="data-instituto-decorator mb-3">NOSOTROS</span>
+                    <h1 class="mb-4">Hidronaval es una institucion de <span class="text-muted">capacitacion.</span></h1>
+                    <h4 class="mb-4">Técnicos que capacitan a mas técnicos</h4>
+                    <p class="text-muted mb-4">Hidronaval  te ofrece una formación de calidad vía e-learning. Los programas de perfeccionamiento profesional están en crecimiento, debido al respaldo a los trabajadores para que aumenten su capacitación y se adapten a los nuevos requerimientos productivos. </p>
+                    <a href="#" class="btn">Ver cursos</a>
+                </div>
+            </div>
+            <div class="col-md-5 order-md-1">
+                <img src="css\img\img-estudios.png" alt="img-estudios">
+            </div>
+        </div>
+        <!--<section class="nosotros d-flex justify-content-evenly">
             <div class="img-info-instituto col-md-5">
                 <img src="css\img\img-estudios.png" alt="img-estudios">
             </div>
@@ -110,7 +124,7 @@
                     <a href="#" class="btn">Ver cursos</a>
                 </div>
             </div>
-        </section>
+        </section>-->
     </div>
 </main>
 
