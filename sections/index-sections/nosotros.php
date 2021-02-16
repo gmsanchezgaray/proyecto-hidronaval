fae13a437f169128f54c1e8633a612b5a63dfe2c
NicolasP <pistillonicolas@gmail.com>
2021-02-16 18:52:55 -0300

    Se modifican nombres de estilos en ingles y reemplaza modulos de sass por clases propias

--- sections/index-sections/nosotros.php
+++ sections/index-sections/nosotros.php
@@ -6,11 +6,11 @@
     </div>
 
     <div class="col-md-6 my-auto info-nos">
-        <span class="data-instituto-decorator">NOSOTROS</span>
-        <h1 class="mb-4">Hidronaval es una institucion de <br> <span class="text-muted">capacitacion.</span></h1>
+        <span class="decorators">NOSOTROS</span>
+        <h1 class="mb-4">Hidronaval es una organización de <span class="text-muted">capacitacion.</span></h1>
         <h4 class="mb-4">Técnicos que capacitan a mas técnicos</h4>
         <p class="text-muted mb-4">Hidronaval  te ofrece una formación de calidad vía e-learning. Los programas de perfeccionamiento profesional están en crecimiento y al respaldo de los trabajadores para que aumenten su capacitación y se adapten a los nuevos requerimientos productivos.</p>
-        <a href="#" class="btn">Ver cursos</a>
+        <a href="#" class="btn hidro-buttons">Ver cursos</a>
     </div>
 
 </div>
