338eda1ad8ab42559c6148d381331ef389ffa14e
Santi <it.santiagotoledo@gmail.com>
2021-02-23 18:26:54 -0300

    fixed hover & active state on skills-buttons

--- sections/index-sections/testimonios.php
+++ sections/index-sections/testimonios.php
@@ -40,18 +40,18 @@
                     </div>
                 </div>
             </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carousel"  data-bs-slide="prev">
-            <span class="material-icons fs-1">
-            chevron_left
-            </span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carousel"  data-bs-slide="next">
-        <span class="material-icons fs-1">
-        chevron_right
-        </span>
-            <span class="visually-hidden">Next</span>
-        </button>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carousel"  data-bs-slide="prev">
+                <span class="material-icons fs-1">
+                    chevron_left
+                </span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel"  data-bs-slide="next">
+                <span class="material-icons fs-1">
+                    chevron_right
+                </span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </div>
-</div>
 </div>
