91d521017fdc30a1b93c1c9df50301b1383a75fa
Santi <it.santiagotoledo@gmail.com>
2021-02-27 19:32:25 -0300

    Responsive for footer and instructor, fixed textarea

--- sections/index-sections/instructor.php
+++ sections/index-sections/instructor.php
@@ -1,17 +1,17 @@
-<div id="instructor" class="teacher-section container pb-6">
-    <div class="row text-center justify-content-center">
+<div id="instructor" class="teacher-section pb-6">
+    <div class="text-center">
         <div class="data-teacher">
             <p class="decorators mb-1">Instructor</p>
             <h5>Docente con años de experiencia impartiendo clases y trabajando en el campo de la Oleohidráulica.</h5>
         </div>
     </div>
 
-    <div class="teacher-works row justify-content-between mt-4">
+    <div class="teacher-works row justify-content-around mt-5">
         <div class="row justify-content-center align-items-center mb-4">
-            <div class="teacher-img-container col-md-5 text-center">
+            <div class="teacher-img-container col-md-4 text-center mb-4">
                 <img class="img-teacher" src="css\img\page-img\img-instructor.jpg" alt="instructor">
             </div>
-            <div class="teacher-items col-md-6 mt-5 m-md-4">
+            <div class="teacher-items col-md-8">
                 <h3 class="fs-2">Dario Omar Pistillo</h3>
 
                 <ul>
@@ -21,40 +21,40 @@
                 </ul>
                 
             </div>
-        </div>
+            <h2 class="text-center">Algunos de sus trabajos</h2>
 
-        <h2 class="text-center">Algunos de sus trabajos</h2>
+            <div class="gallery">
 
-        <div class="col gallery">
+                <div class="gallery-item">
+                    <img src="css\img\galeria-trabajos\img-polaris.png" alt="img-polaris" class="img-gallery-works">
+                    <p class="position-absolute">Click en la imagen para ver informacion</p>
+                </div>
+                
+                <div class="gallery-item position-relative">
+                    <img src="css\img\galeria-trabajos\Rubimar.jpg" alt="prueba-1" class="img-gallery-works">
+                    <p class="position-absolute">Click en la imagen para ver informacion</p>
+                </div>
 
-            <div class="gallery-item">
-                <img src="css\img\galeria-trabajos\img-polaris.png" alt="img-polaris" class="img-gallery-works">
-                <p class="position-absolute">Click en la imagen para ver informacion</p>
-            </div>
-            
-            <div class="gallery-item position-relative">
-                <img src="css\img\galeria-trabajos\Rubimar.jpg" alt="prueba-1" class="img-gallery-works">
-                <p class="position-absolute">Click en la imagen para ver informacion</p>
-            </div>
+                <div class="gallery-item">
+                    <img src="css\img\galeria-trabajos\San Julian.JPG" alt="prueba-2" class="img-gallery-works">
+                    <p class="position-absolute">Click en la imagen para ver informacion</p>
+                </div>
 
-            <div class="gallery-item">
-                <img src="css\img\galeria-trabajos\San Julian.JPG" alt="prueba-2" class="img-gallery-works">
-                <p class="position-absolute">Click en la imagen para ver informacion</p>
-            </div>
+                <div class="gallery-item">
+                    <img src="css\img\galeria-trabajos\San Matias.JPG" alt="prueba-3" class="img-gallery-works">
+                    <p class="position-absolute">Click en la imagen para ver informacion</p>
+                </div>
 
-            <div class="gallery-item">
-                <img src="css\img\galeria-trabajos\San Matias.JPG" alt="prueba-3" class="img-gallery-works">
-                <p class="position-absolute">Click en la imagen para ver informacion</p>
-            </div>
+                <div class="gallery-item">
+                    <img src="css\img\galeria-trabajos\img-prueba-5.jpg" alt="prueba-5" class="img-gallery-works">
+                    <p class="position-absolute">Click en la imagen para ver informacion</p>
+                </div>
 
-            <div class="gallery-item">
-                <img src="css\img\galeria-trabajos\img-prueba-5.jpg" alt="prueba-5" class="img-gallery-works">
-                <p class="position-absolute">Click en la imagen para ver informacion</p>
-            </div>
+                <div class="gallery-item">
+                    <img src="css\img\galeria-trabajos\img-prueba-6.jpg" alt="prueba-6" class="img-gallery-works">
+                    <p class="position-absolute">Click en la imagen para ver informacion</p>
+                </div>
 
-            <div class="gallery-item">
-                <img src="css\img\galeria-trabajos\img-prueba-6.jpg" alt="prueba-6" class="img-gallery-works">
-                <p class="position-absolute">Click en la imagen para ver informacion</p>
             </div>
 
         </div>
